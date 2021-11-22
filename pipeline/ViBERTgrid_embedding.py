eb205e387c03885eb0cd17aad639c1a82665fa46
Zening Lin <zening.lin@outlook.com>
2021-11-22 12:18:39 +0800

    <opti> implement token average/take_first

--- pipeline/ViBERTgrid_embedding.py
+++ pipeline/ViBERTgrid_embedding.py
@@ -10,10 +10,31 @@ def BERT_embedding(
     bert_model: Callable = None,
     mode='train'
 ) -> torch.tensor:
+    """apply BERT embedding to the given corpus 
+     
+    corpus and mask directly come from the SROIEDataset through DataLoader,   
+    padding and sliding windows will be applied automatically in this function
+
+    Parameters
+    ----------
+    corpus : torch.Tensor
+        corpus from SROIEDataset
+    mask : torch.Tensor
+        mask from SROIEDataset, indicates whether the token is valid or not
+    bert_model : Callable, optional
+        bert_model in hugging face, by default None
+    mode : str, optional
+        'train' or 'eval' to control back-propagation, by default 'train'
+
+    Returns
+    -------
+    BERT_embeddings : torch.tensor
+        BERT embeddings of the corpus, same in shape with corpus
+    """
 
     assert bert_model is not None, 'no bert model given'
     assert mode in [
-        'train', 'eval'], 'invalid mode \'{}\', must be \'train\' or \'eval\''.format(mode)
+        'train', 'eval'], f"invalid mode '{mode}', must be 'train' or 'eval'"
     model = bert_model
 
     if(mode == 'train'):
@@ -83,22 +104,80 @@ def BERT_embedding(
 def ViBERTgrid_embedding(
     BERT_embeddings: torch.Tensor,
     coors: torch.Tensor,
+    mask: torch.Tensor,
     img_shape: Tuple,
-    stride: int = 8
+    stride: int = 8,
+    mode: str = 'mean'
 ) -> torch.Tensor:
+    """map the given BERT embeddings to the ViBERTgrid
+
+    Parameters
+    ----------
+    BERT_embeddings : torch.Tensor
+        BERT embeddings from the BERT_embedding function
+    coors : torch.Tensor
+        coordinate tensor from the SROIEDataset
+    mask : torch.Tensor
+        mask tensor from the SROIEDataset
+    img_shape : Tuple
+        shape of input features of ResNet18
+    stride : int, optional
+        stride of the feature map at early fusion, by default 8
+    mode : str, optional
+        ViBERTgrid tokens embedding mode.
+        words from the OCR result were splited into several tokens through tokenizer,  
+        at ViBERTgrid embedding step, measures shall be taken to  
+        aggregate these token embeddings back into word-level,  
+        'mean' mode average token embeddings from the same word,  
+        'first' mode take the first token embeddings of a word,  
+        by default 'mean'
+
+    Returns
+    -------
+    ViBERTgrid : torch.Tensor
+        ViBERTgrid embeddings
+    """
+    
+    assert mode in [
+        'mean', 'first'], f"mode should be 'mean' or 'first', {mode} were given"
+
     bs, seq_len, num_dim = BERT_embeddings.shape
+
     ViBERTgrid = torch.zeros(
         (bs, num_dim, int(img_shape[0] / stride), int(img_shape[1] / stride)))
-    for seq_index in range(seq_len):
-        curr_coors = coors[:, seq_index, :] / stride
-        curr_coors = curr_coors.int()
-        curr_emb = BERT_embeddings[:, seq_index, :]
-        for bs_index in range(bs):
-            ViBERTgrid[bs_index, :,
-                       curr_coors[bs_index, 1]: curr_coors[bs_index, 3],
-                       curr_coors[bs_index, 0]: curr_coors[bs_index, 2]] = \
-                curr_emb[bs_index, :, None,
-                         None]  # expand dimensions to match requirements
+
+    for bs_index in range(bs):
+        # TODO  low efficiency implementation, may optimized later
+        #       1. unable to vectorize due to the corpus length difference inside a batch
+        
+        # initialize coors with [-1, -1, -1, -1] to match first coor
+        prev_coors = torch.ones((coors.shape[2]), dtype=torch.long) * (-1)
+        if mode == 'mean':
+            mean_count = 1
+            curr_embs = torch.zeros((num_dim), dtype=torch.float32)
+        for seq_index in range(seq_len):
+            if mask[bs_index, seq_index] == 0:
+                break
+            if coors[bs_index, seq_index, :].equal(prev_coors):
+                if mode == 'mean':
+                    curr_embs += BERT_embeddings[bs_index, seq_index, :]
+                    mean_count += 1
+                elif mode == 'first':
+                    continue
+            else:
+                word_coors = coors[bs_index, seq_index] / stride
+                word_coors = word_coors.int()
+
+                if mode == 'mean':
+                    ViBERTgrid[bs_index, :, word_coors[1]: word_coors[3], word_coors[0]: word_coors[2]] = \
+                        (curr_embs[:, None, None] / mean_count)
+                    mean_count = 1
+                    curr_embs = BERT_embeddings[bs_index, seq_index, :]
+                elif mode == 'first':
+                    ViBERTgrid[bs_index, :, word_coors[1]: word_coors[3], word_coors[0]: word_coors[2]] = \
+                        BERT_embeddings[bs_index, seq_index, :, None, None]
+
+            prev_coors = coors[bs_index, seq_index, :]
 
     return ViBERTgrid
 
@@ -132,6 +211,6 @@ if __name__ == '__main__':
     for img, class_label, pos_neg_label, coor, corpus, mask in train_loader:
         BERTemb = BERT_embedding(corpus, mask, model)
         ViBERTgrid = ViBERTgrid_embedding(
-            BERTemb, coor, RESIZE_SHAPE, stride=8)
+            BERTemb, coor, mask, RESIZE_SHAPE, stride=8, mode='mean')
         print(ViBERTgrid.shape)
         ViBERTgrid_visualize(ViBERTgrid)
