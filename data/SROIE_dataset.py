de217da67baea3ca7e761825d3f47f501c619af6
Zening Lin <zening.lin@outlook.com>
2021-12-17 17:39:01 +0800

    <opti>implement memory friendly loader

--- data/SROIE_dataset.py
+++ data/SROIE_dataset.py
@@ -13,9 +13,6 @@ from torch.utils.data import Dataset, DataLoader, random_split
 from transformers import BertTokenizer
 
 
-RESIZE_SHAPE = (336, 256)
-
-
 class SROIEDataset(Dataset):
     def __init__(
         self,
@@ -34,56 +31,81 @@ class SROIEDataset(Dataset):
         # self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', add_special_tokens = True)
         self.max_length = 0
         self.transform_img = torchvision.transforms.Compose([
-            torchvision.transforms.Resize(
-                RESIZE_SHAPE,
-                interpolation=torchvision.transforms.InterpolationMode.NEAREST
-            ),
             torchvision.transforms.ToTensor()
         ])
 
-        if self.train:
-            self.img_list, self.class_list, self.pos_neg_list, self.ocr_result_list = self._extract_train(
-                self.root)
+        dir_img = os.path.join(root, 'image')
+        self.filename_list = [f for f in os.listdir(dir_img)]
 
     def __len__(self) -> int:
-        return len(self.img_list)
+        return len(self.filename_list)
 
-    # @pysnooper.snoop()
     def __getitem__(self, index):
-        if self.train:
-            return_ocr_result = self.ocr_result_list[index]
-            ocr_coor = []
-            ocr_text = []
-            for item in return_ocr_result:
-                curr_text = item[4:]
-                # avoid " ' , in ocr recognition results affect list splitting
-                curr_text = ''.join(curr_text)
-                curr_text.replace('\"', '')
-                if curr_text == '':
-                    continue        # discard empty results
-                ocr_coor.append(list(map(int, item[:4])))
-                ocr_text.append(curr_text)
+        dir_img = os.path.join(self.root, 'image')
+        dir_class = os.path.join(self.root, 'class')
+        dir_pos_neg = os.path.join(self.root, 'pos_neg')
+        dir_ocr_result = os.path.join(self.root, 'ocr_result')
 
-            ocr_coor_expand = []
-            ocr_tokens = []
-            for text, coor in zip(ocr_text, ocr_coor):
-                if text == '':
-                    continue
-                curr_tokens = self.tokenizer.tokenize(text)
-                for i in range(len(curr_tokens)):
-                    ocr_coor_expand.append(coor)
-                    ocr_tokens.append(curr_tokens[i])
+        file = self.filename_list[index]
 
-            ocr_corpus = self.tokenizer.convert_tokens_to_ids(ocr_tokens)
+        image = Image.open(os.path.join(dir_img, file))
 
-            return (self.transform_img(self.img_list[index]),
-                    torch.tensor(self.class_list[index]),
-                    torch.tensor(self.pos_neg_list[index]),
-                    torch.tensor(ocr_coor_expand, dtype=torch.long),
-                    torch.tensor(ocr_corpus, dtype=torch.long))
+        data_class = np.load(os.path.join(
+            dir_class, file.replace('jpg', 'npy')))
 
-    # @pysnooper.snoop()
-    def _ViBERTgrid_coll_func(self, samples):
+        pos_neg = np.load(os.path.join(
+            dir_pos_neg, file.replace('jpg', 'npy')))
+
+        with open(os.path.join(dir_ocr_result, file.replace('jpg', 'csv')), 'r', encoding='utf-8') as csv_file:
+            data_lines = csv_file.readlines()[1:]
+            self.max_length = len(data_lines) if (
+                len(data_lines) > self.max_length) else self.max_length
+            # # debug----------------------------------------------------
+            # curr_ocr_result = [data_line.strip().split(',')[1:]
+            #                     for data_line in data_lines]
+            # if(len(curr_ocr_result) == 0):
+            #     print('\nempty result found in data extraction, please check')
+            #     print(file.replace('.jpg', 'csv'))
+            # # ---------------------------------------------------------
+            ocr_result = [data_line[:-2].strip().split(',')[1:]
+                          for data_line in data_lines]
+
+        ocr_coor = []
+        ocr_text = []
+        for item in ocr_result:
+            curr_text = item[4:]
+            # avoid " ' , in ocr recognition results affect list splitting
+            curr_text = ''.join(curr_text)
+            curr_text.replace('\"', '')
+            if curr_text == '':
+                continue        # discard empty results
+            ocr_coor.append(list(map(int, item[:4])))
+            ocr_text.append(curr_text)
+
+        ocr_coor_expand = []
+        ocr_tokens = []
+        for text, coor in zip(ocr_text, ocr_coor):
+            if text == '':
+                continue
+            curr_tokens = self.tokenizer.tokenize(text)
+            for i in range(len(curr_tokens)):
+                ocr_coor_expand.append(coor)
+                ocr_tokens.append(curr_tokens[i])
+
+        ocr_corpus = self.tokenizer.convert_tokens_to_ids(ocr_tokens)
+
+        return (self.transform_img(image),
+                torch.tensor(data_class),
+                torch.tensor(pos_neg),
+                torch.tensor(ocr_coor_expand, dtype=torch.long),
+                torch.tensor(ocr_corpus, dtype=torch.long))
+
+    @staticmethod
+    def _coll_func(batch):
+        return tuple(zip(*batch))
+
+    @staticmethod
+    def _ViBERTgrid_coll_func(samples):
         imgs = []
         class_labels = []
         pos_neg_labels = []
@@ -103,7 +125,7 @@ class SROIEDataset(Dataset):
         mask = torch.zeros(ocr_corpus.shape, dtype=torch.long)
         mask = mask.masked_fill_((ocr_corpus != 0), 1)
 
-        return (torch.stack(imgs, dim=0),
+        return (tuple(imgs),
                 torch.stack(class_labels, dim=0),
                 torch.stack(pos_neg_labels, dim=0),
                 ocr_coors,
@@ -111,7 +133,7 @@ class SROIEDataset(Dataset):
                 mask)
 
     def _extract_train(self, root: str):
-        dir_img = os.path.join(root, 'img')
+        dir_img = os.path.join(root, 'image')
         dir_class = os.path.join(root, 'class')
         dir_pos_neg = os.path.join(root, 'pos_neg')
         dir_ocr_result = os.path.join(root, 'ocr_result')
@@ -158,7 +180,6 @@ class SROIEDataset(Dataset):
 def load_train_dataset(
     root: str,
     batch_size: int,
-    val_ratio: float = 0.3,
     num_workers: int = 0,
     tokenizer: Optional[Callable] = None
 ) -> Tuple[DataLoader]:
@@ -170,8 +191,6 @@ def load_train_dataset(
         root of dataset
     batch_size : int
         batch size
-    val_ratio : float, optional
-        ratio of validation subset, by default 0.3
     num_workers : int, optional
         number of workers in dataloader, by default 0
     tokenizer : optional
@@ -183,19 +202,16 @@ def load_train_dataset(
     val_loader : DataLoader
 
     """
-    SROIE_dataset = SROIEDataset(root, train=True, tokenizer=tokenizer)
-    len_val = int(len(SROIE_dataset) * val_ratio)
-    len_train = len(SROIE_dataset) - len_val
-    train_dataset, val_dataset = random_split(
-        SROIE_dataset, [len_train, len_val])
+    SROIE_train_dataset = SROIEDataset(root, train=True, tokenizer=tokenizer)
+    SROIE_val_dataset = SROIEDataset(root, train=False, tokenizer=tokenizer)
 
     train_loader = DataLoader(
-        train_dataset, batch_size=batch_size, shuffle=True,
-        num_workers=num_workers, collate_fn=SROIE_dataset._ViBERTgrid_coll_func)
+        SROIE_train_dataset, batch_size=batch_size, shuffle=True,
+        num_workers=num_workers, collate_fn=SROIE_train_dataset._ViBERTgrid_coll_func)
 
     val_loader = DataLoader(
-        val_dataset, batch_size=batch_size, shuffle=True,
-        num_workers=num_workers, collate_fn=SROIE_dataset._ViBERTgrid_coll_func)
+        SROIE_val_dataset, batch_size=batch_size, shuffle=True,
+        num_workers=num_workers, collate_fn=SROIE_val_dataset._ViBERTgrid_coll_func)
 
     return train_loader, val_loader
 
@@ -208,7 +224,6 @@ if __name__ == '__main__':
     train_loader, val_loader = load_train_dataset(
         dir_processed,
         batch_size=4,
-        val_ratio=0.3,
         num_workers=0,
         tokenizer=tokenizer
     )
