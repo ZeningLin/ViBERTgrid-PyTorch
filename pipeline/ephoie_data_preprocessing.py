8a9fc64fc9dca90c58d1e8ab1add658e2201e001
Zening Lin <zening.lin@outlook.com>
2022-03-10 20:24:26 +0800

    <opti> use ltp instead of char-level word splitting for EPHOIE

--- pipeline/ephoie_data_preprocessing.py
+++ pipeline/ephoie_data_preprocessing.py
@@ -6,6 +6,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from ltp import LTP
+
 from shutil import copy
 from tqdm import tqdm
 
@@ -60,6 +62,91 @@ def generate_json(root_dir_txt_label: str, root_dir_json_label: str) -> None:
                 os.path.join(root_dir_json_label, file.replace("txt", "json"))
             ),
         )
+
+
+def single_label_parser_ltp(
+    dir_img: str,
+    dir_json_label: str,
+    dir_csv_label: str,
+    dir_class: str,
+    dir_pos_neg: str,
+    target_shape: Tuple[int] = None,
+):
+    image = plt.imread(dir_img)
+    image_shape = image.shape
+
+    if target_shape is not None:
+        assert (
+            len(target_shape) == 2
+        ), f"target_shape can only contain 2 elements, {len(target_shape)} given"
+        pos_neg_label = np.zeros(target_shape, dtype=int)
+        class_label = np.zeros(target_shape, dtype=int)
+    else:
+        pos_neg_label = np.zeros((image_shape[0], image_shape[1]), dtype=int)
+        class_label = np.zeros((image_shape[0], image_shape[1]), dtype=int)
+
+    csv_label = pd.DataFrame(
+        columns=["left", "top", "right", "bot", "text", "data_class", "pos_neg"]
+    )
+
+    ltp_splitter = LTP()
+
+    with open(dir_json_label, "r") as json_f:
+        json_label: Dict = json.load(json_f)
+
+        for segment in json_label.values():
+            splitted, _ = ltp_splitter.seg([segment["string"]])
+            num_char = len(segment["string"])
+            num_seg = len(splitted[0])
+            hor_candidate = segment["box"][::2]
+            ver_candidate = segment["box"][1::2]
+
+            left_coor = int(min(hor_candidate))
+            top_coor = int(min(ver_candidate))
+            right_coor = int(max(hor_candidate))
+            bot_coor = int(max(ver_candidate))
+            width = right_coor - left_coor
+
+            if target_shape is not None:
+                scale_x = target_shape[0] / image_shape[0]
+                scale_y = target_shape[1] / image_shape[1]
+                left_coor *= scale_x
+                right_coor *= scale_x
+                top_coor *= scale_y
+                bot_coor *= scale_y
+
+            char_width = (width + num_char - 1) // num_char
+            curr_left = left_coor
+            char_index = 0
+            for seg_index in range(num_seg):
+                num_curr_char = len(splitted[0][seg_index])
+                curr_right = curr_left + char_width * num_curr_char
+                seg_class = segment["tag"][char_index]
+                char_pos_neg = 2 if (seg_class == 0) else 1
+
+                class_label[top_coor:bot_coor, curr_left:curr_right] = seg_class
+                pos_neg_label[top_coor:bot_coor, curr_left:curr_right] = char_pos_neg
+
+                curr_row_dict = {
+                    "left": [curr_left],
+                    "top": [top_coor],
+                    "right": [curr_right],
+                    "bot": [bot_coor],
+                    "text": [splitted[0][seg_index]],
+                    "data_class": [seg_class],
+                    "pos_neg": [char_pos_neg],
+                }
+                curr_row_dataframe = pd.DataFrame(curr_row_dict)
+                csv_label = pd.concat(
+                    [csv_label, curr_row_dataframe], axis=0, ignore_index=True
+                )
+
+                curr_left = curr_right
+                char_index += num_curr_char
+
+    np.save(dir_class, class_label)
+    np.save(dir_pos_neg, pos_neg_label)
+    csv_label.to_csv(dir_csv_label)
 
 
 def single_label_parser(
@@ -169,7 +256,7 @@ def data_preprocessing_pipeline(
         dir_csv_label = os.path.join(root_dir_csv_label, file.replace("jpg", "csv"))
         dir_class = os.path.join(root_dir_class, file.replace("jpg", "npy"))
         dir_pos_neg = os.path.join(root_dir_pos_neg, file.replace("jpg", "npy"))
-        single_label_parser(
+        single_label_parser_ltp(
             dir_img=dir_image,
             dir_json_label=dir_json_label,
             dir_csv_label=dir_csv_label,
@@ -180,12 +267,12 @@ def data_preprocessing_pipeline(
 
 
 if __name__ == "__main__":
-    image_root = r"dir_to_root/EPHOIE/image"
-    txt_label_root = r"dir_to_root/EPHOIE/label"
-    json_label_root = r"dir_to_root/EPHOIE/_label_json"
-    csv_label_root = r"dir_to_root/EPHOIE/_label_csv"
-    class_label_root = r"dir_to_root/EPHOIE/_class"
-    pos_neg_label_root = r"dir_to_root/EPHOIE/_pos_neg"
+    image_root = r"/media/zening_lin@intsig.com/sdb/datasets/EPHOIE_ltp/image"
+    txt_label_root = r"/media/zening_lin@intsig.com/sdb/datasets/EPHOIE_ltp/label"
+    json_label_root = r"/media/zening_lin@intsig.com/sdb/datasets/EPHOIE_ltp/_label_json"
+    csv_label_root = r"/media/zening_lin@intsig.com/sdb/datasets/EPHOIE_ltp/_label_csv"
+    class_label_root = r"/media/zening_lin@intsig.com/sdb/datasets/EPHOIE_ltp/_class"
+    pos_neg_label_root = r"/media/zening_lin@intsig.com/sdb/datasets/EPHOIE_ltp/_pos_neg"
 
     if not os.path.exists(json_label_root):
         generate_json(
